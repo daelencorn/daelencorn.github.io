6e9758a479b6b06b909171e6143289cb466238ba
daelencorn <daelencorn@gmail.com>
2015-06-17 02:55:12 -0700

    Create cross-domain-request.js

---
+++ src/cross-domain-request.js
@@ -0,0 +1,28 @@
+function requestCrossDomain( site, callback ) {
+
+  if ( !site) {
+     alert('no site found')
+     return false;
+  }
+
+  var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + site + '"') 
+  + '&format=xml&callback=?';
+  
+  $.getJSON(yql, function(data) {
+  
+      if ( data.results[0] ) {
+      
+          data = data.results[0].replace(/<script[^>]*>[/s/S]*?<\/script>/, '');
+      
+          if (typeof callback === 'function' {
+            callback(data);
+          }
+      
+      }
+      
+      else throw new Error('Nothing returned');
+      
+      
+  });
+
+}
